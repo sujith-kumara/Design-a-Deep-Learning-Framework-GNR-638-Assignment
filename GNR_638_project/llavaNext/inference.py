23602650623d63fbd094fc0194403cf3055b112b
anuroopck <79464093+anuroopck@users.noreply.github.com>
2026-05-02 21:59:27 +0530

    Add files via upload

--- GNR_638_project/llavaNext/inference.py
+++ GNR_638_project/llavaNext/inference.py
@@ -218,7 +218,7 @@ def load_model():
     model = LlavaNextForConditionalGeneration.from_pretrained(
         MODEL_NAME, 
         torch_dtype=torch.float16,
-        quantization_config=quantization_config,
+#        quantization_config=quantization_config,
         low_cpu_mem_usage=True,
         device_map="auto" 
     )
@@ -295,7 +295,7 @@ def run_pipeline(image_path: str, csv_path: str, output_path: str):
         num_ans = answer_question(processor, model, image, row["question"], options)
         print(f"[{idx+1:02d}] pred={num_ans}")
         
-        question_id = row["question_id"]
+        question_id = f"ques_{row['question_id']}" if str(row["question_id"]).isdigit() else str(row["question_id"])
         results.append({
             "id": question_id, 
             "question_num": question_id,
